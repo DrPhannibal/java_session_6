a5cb762a009f86b6c58e344f679c1afb26ad2857
DrPhannibal <mahdisouri0@gmail.com>
2022-04-27 00:13:16 +0430

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+       int i=5;
+
+//       while (i<10){
+//           System.out.println(i);
+//           i++;
+//       }
+//        System.out.println("exit");
+
+       do {
+           System.out.println(++i);
+       }while (i<5);
+
+
+       for (int j=0;j<10;j++){
+           System.out.println(j);
+       }
+    }
+}
